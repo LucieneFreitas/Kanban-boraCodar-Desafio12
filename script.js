f8f6ec573dcd2207f0d6286e3d31ad4fdebef131
luciene <luciene_freitas@yahoo.com.br>
2023-03-30 08:33:50 -0300

    git add .

---
+++ script.js
@@ -0,0 +1,12 @@
+/*
+
+const elements = document.querySelectorAll('.card p')
+const LIMIT = 60
+
+for (let p of elements) {
+    const aboveLimit = p.innerText.length > LIMIT
+    const dotsOrEmpty = aboveLimit ? '...' : ''
+    p.innerText = p.innerText.substring(0, LIMIT) + dotsOrEmpty
+}
+
+*/
